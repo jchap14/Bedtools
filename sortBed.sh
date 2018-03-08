43f87db4ebe328868ed0e99b8878aa49aa2b9446
James <jchap14@gmail.com>
2018-03-07 19:18:47 -0800

    initial

---
+++ sortBed.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##### Use this script to sort all bedfiles using bedtools
+
+##### submission command
+## for x in `/bin/ls *.bed`; do bash sortBed.sh $x; done
+
+##### set variables
+BEDFILE=$1
+NAME=`basename $BEDFILE .bed`
+
+##### write tempscripts for each
+cat > $NAME.tempscript.sh << EOF
+#!/bin/bash
+## run the bedtools sort command
+echo "removing quotes & sorting $BEDFILE"
+cat $BEDFILE | tr -d '"' | bedtools sort -i stdin > $NAME.sorted.bed
+
+## change name back to original
+# mv $NAME.sorted.bed $NAME.bed
+EOF
+
+##### bash then remove the tempscript
+bash $NAME.tempscript.sh
+sleep 1
+rm $NAME.tempscript.sh
