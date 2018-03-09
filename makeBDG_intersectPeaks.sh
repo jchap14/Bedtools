df778d7142329d72f16f276404a16b656a158a3c
James <jchap14@gmail.com>
2018-03-09 00:39:45 -0800

    a

--- makeBDG_intersectPeaks.sh
+++ makeBDG_intersectPeaks.sh
@@ -1,16 +1,14 @@
-
-
 #!/bin/bash
 ##### Use this script to sort all bedfiles using bedtools
 
 ##### submission command
-##        bash makeBDG_intersectPeaks.sh $PEAKFILE $BEDFILE; done
-## e.g.:  bash makeBDG_intersectPeaks.sh SLO.union.peaks.bed 
+##        bash makeBDG_intersectPeaks.sh $PEAKFILE $BEDFILE $OUTNAME; done
+## e.g.:  bash makeBDG_intersectPeaks.sh SLO.union.peaks.bed LS.subSamp2.bed LSxSLO
 
 ##### set variables
 PEAKFILE=$1
 BEDFILE=$2
-NAME=`basename $BEDFILE .bed`
+OUTPREFIX=$3
 CHROMSIZES=/srv/gsfs0/projects/snyder/chappell/Annotations/GENCODE-v19-GRCh37-hg19/hg19.chrom.sizes
 
 ##### write tempscripts for each
@@ -19,16 +17,16 @@ cat > $NAME.tempscript.sh << EOF
 
 ## run the bedtools genomecov command to calculate the coverage into bedGraph format
 echo "run the bedtools genomecov command to calculate the coverage into bedGraph format"
-bedtools genomecov -bg -i $BEDFILE -g $CHROMSIZES > $NAME.bdg
+bedtools genomecov -bg -i $BEDFILE -g $CHROMSIZES > $OUTPREFIX.hg19.bdg
 
-## run the bedtools sort command
-echo "removing quotes & sorting $BEDFILE"
-bedtools intersect -a $BEDFILE -b $PEAKFILE
+## intersect the bedGraph with regions of interest (e.g. peaks)
+echo "intersecting bedGraph with regions of interest"
+bedtools intersect -a $BEDFILE -b $PEAKFILE > $OUTPREFIX.avgSigUnionPeaks.bdg
 
-
+echo "Done!"
 EOF
 
 ##### bash then remove the tempscript
 bash $NAME.tempscript.sh
 sleep 1
-rm $NAME.tempscript.sh
+# rm $NAME.tempscript.sh
