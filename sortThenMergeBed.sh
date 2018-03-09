aa55cc2deeb96d3bfb5c082e67fbb33a14ce4d7b
James <jchap14@gmail.com>
2018-03-08 20:13:37 -0800

    a

--- sortThenMergeBed.sh
+++ sortThenMergeBed.sh
@@ -17,7 +17,7 @@ cat > $NAME.tempscript.sh << EOF
 #$ -j y
 #$ -cwd
 #$ -V
-#$ -l h_vmem=1G
+#$ -l h_vmem=4G
 #$ -pe shm 12
 #$ -l h_rt=5:59:00
 #$ -l s_rt=5:59:00
@@ -27,7 +27,7 @@ cat `cat $BEDLIST | tr '\n' ' '` > $NAME.catBED
 ## run the bedtools sort command
 echo "removing quotes & sorting $BEDFILE"
 cat $NAME.catBED | tr -d '"' | bedtools sort -i stdin > $NAME.sorted.bed
-mergeBed -i $NAME.sorted.bed > $NAME.avg.bed
+mergeBed -s -i $NAME.sorted.bed > $NAME.avg.bed
 #
 EOF
 
