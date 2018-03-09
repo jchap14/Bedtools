db98ba9eab92546ba1d969fb3e54e2bf2bc902ca
James <jchap14@gmail.com>
2018-03-09 01:56:32 -0800

    a

--- sortBed.sh
+++ sortBed.sh
@@ -11,6 +11,15 @@ NAME=`basename $BEDFILE .bed`
 ##### write tempscripts for each
 cat > $NAME.tempscript.sh << EOF
 #!/bin/bash
+#$ -N $NAME.bedSort
+#$ -j y
+#$ -cwd
+#$ -V
+#$ -l h_vmem=4G
+#$ -pe shm 12
+#$ -l h_rt=5:59:00
+#$ -l s_rt=5:59:00
+
 ## run the bedtools sort command
 echo "removing quotes & sorting $BEDFILE"
 cat $BEDFILE | tr -d '"' | sort -k 1,1 -k2,2n > $NAME.sorted.bed
@@ -20,6 +29,6 @@ cat $BEDFILE | tr -d '"' | sort -k 1,1 -k2,2n > $NAME.sorted.bed
 EOF
 
 ##### bash then remove the tempscript
-bash $NAME.tempscript.sh
+qsub $NAME.tempscript.sh
 sleep 1
 rm $NAME.tempscript.sh
