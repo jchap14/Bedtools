da19c352d41a18ce6cdeb32512e7ee8fbbfa0aea
James <jchap14@gmail.com>
2018-04-04 12:53:23 -0700

    autofind bigwigs

--- bigWig_correlation.sh
+++ bigWig_correlation.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-WIG1="/srv/gsfs0/projects/snyder/chappell/JR/SLO/CUT_n_RUN/2017.10.25/peak_calling/GFP_K27me3.nmSort.bw"
-WIG2="/srv/gsfs0/projects/snyder/chappell/JR/SLO/CUT_n_RUN/2017.10.25/peak_calling/MEK5_K27me3.nmSort.bw"
-WIG3="/srv/gsfs0/projects/snyder/chappell/JR/SLO/CUT_n_RUN/original_set/GFP__K27me3.nmSort.bw"
-WIG4="/srv/gsfs0/projects/snyder/chappell/JR/SLO/CUT_n_RUN/original_set/MEK5_K27me3.nmSort.bw"
+##### bash bigWig_correlation.sh
 
-wigCorrelate $WIG1 $WIG2 $WIG3 $WIG4 > CnR_bigwig_correlation.txt
+wigCorrelate `find *.nmSort.bw | tr '\n' ' '` > bigWig_correlation.txt
