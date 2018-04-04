39ade5b1c2900d8d70c603b91f668148a5f8f025
James <jchap14@gmail.com>
2018-04-04 13:00:34 -0700

    f

--- bigWig_correlation.sh
+++ bigWig_correlation.sh
@@ -2,4 +2,27 @@
 
 ##### bash bigWig_correlation.sh
 
-wigCorrelate `find *.nmSort.bw | tr '\n' ' '` > bigWig_correlation.txt
+## define variables
+BIGWIGS=`find *.nmSort.bw | tr '\n' ' '`
+
+## write a tempscript to be looped over
+cat > BWcorr.tempscript.sh << EOF
+#!/bin/bash
+#$ -N $NAME.BWcorr
+#$ -j y
+#$ -cwd
+#$ -V
+#$ -l h_vmem=4G
+#$ -pe shm 1
+#$ -l h_rt=0:59:00
+#$ -l s_rt=0:59:00
+
+##### run commands
+echo "correlating bigwigs"
+wigCorrelate $BIGWIGS > bigWig_correlation.txt
+EOF
+
+## qsub then remove the tempscript
+bash BWcorr.tempscript.sh 
+sleep 1
+rm BWcorr.tempscript.sh
